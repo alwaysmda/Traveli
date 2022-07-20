9fb3e931b029c16ffe9a7919cee80d39b2972a86
mohamad <mohamadkhalili793@gmail.com>
2022-07-20 11:01:30 +0430

    Add TravelDetailAdapter.kt

---
+++ app/src/main/java/adapter/TravelDetailAdapter.kt
@@ -0,0 +1,162 @@
+package adapter
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.xodus.templatefive.R
+import com.xodus.templatefive.databinding.*
+import domain.model.travel.TravelDetail
+import domain.model.travel.TravelDetail.Companion.VIEW_TYPE_BOOKMARK
+import domain.model.travel.TravelDetail.Companion.VIEW_TYPE_COVER
+import domain.model.travel.TravelDetail.Companion.VIEW_TYPE_DESCRIPTION
+import domain.model.travel.TravelDetail.Companion.VIEW_TYPE_IMAGE
+import domain.model.travel.TravelDetail.Companion.VIEW_TYPE_LINK
+import domain.model.travel.TravelDetail.Companion.VIEW_TYPE_VIDEO
+
+
+class TravelDetailDetailAdapter : ListAdapter<TravelDetail, RecyclerView.ViewHolder>(DiffCallback()) {
+
+
+    inner class CoverViewHolder(private val binding: RowTravelDetailCoverBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(travelDetail: TravelDetail) {
+
+        }
+    }
+
+    inner class ImageViewHolder(private val binding: RowTravelDetailImageBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(travelDetail: TravelDetail) {
+
+        }
+    }
+
+    inner class DescriptionViewHolder(private val binding: RowTravelDetailDescriptionBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(travelDetail: TravelDetail) {
+
+        }
+    }
+
+    inner class LinkViewHolder(private val binding: RowTravelDetailLinkBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(travelDetail: TravelDetail) {
+
+        }
+    }
+
+    inner class VideoViewHolder(private val binding: RowTravelDetailVideoBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(travelDetail: TravelDetail) {
+
+        }
+    }
+
+    inner class BookMarkViewHolder(private val binding: RowTravelDetailBookmarkBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(travelDetail: TravelDetail) {
+
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_COVER       -> {
+                CoverViewHolder(
+                    DataBindingUtil.inflate(
+                        LayoutInflater.from(parent.context),
+                        R.layout.row_travel_detail_cover,
+                        parent,
+                        false
+                    )
+                )
+
+            }
+            VIEW_TYPE_IMAGE       -> {
+                ImageViewHolder(
+                    DataBindingUtil.inflate(
+                        LayoutInflater.from(parent.context),
+                        R.layout.row_travel_detail_image,
+                        parent,
+                        false
+                    )
+                )
+
+            }
+            VIEW_TYPE_DESCRIPTION -> {
+                DescriptionViewHolder(
+                    DataBindingUtil.inflate(
+                        LayoutInflater.from(parent.context),
+                        R.layout.row_travel_detail_description,
+                        parent,
+                        false
+                    )
+                )
+            }
+            VIEW_TYPE_LINK        -> {
+                LinkViewHolder(
+                    DataBindingUtil.inflate(
+                        LayoutInflater.from(parent.context),
+                        R.layout.row_travel_detail_link,
+                        parent,
+                        false
+                    )
+                )
+            }
+            VIEW_TYPE_VIDEO       -> {
+                VideoViewHolder(
+                    DataBindingUtil.inflate(
+                        LayoutInflater.from(parent.context),
+                        R.layout.row_travel_detail_video,
+                        parent,
+                        false
+                    )
+                )
+
+            }
+            VIEW_TYPE_BOOKMARK    -> {
+                BookMarkViewHolder(
+                    DataBindingUtil.inflate(
+                        LayoutInflater.from(parent.context),
+                        R.layout.row_travel_detail_bookmark,
+                        parent,
+                        false
+                    )
+                )
+
+            }
+            else                  -> ImageViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.row_travel_detail_image,
+                    parent,
+                    false
+                )
+            )
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return currentList[position].viewType
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is CoverViewHolder       -> holder.bind(currentList[position])
+            is ImageViewHolder       -> holder.bind(currentList[position])
+            is DescriptionViewHolder -> holder.bind(currentList[position])
+            is LinkViewHolder        -> holder.bind(currentList[position])
+            is VideoViewHolder       -> holder.bind(currentList[position])
+            is BookMarkViewHolder    -> holder.bind(currentList[position])
+        }
+    }
+
+
+    private class DiffCallback : DiffUtil.ItemCallback<TravelDetail>() {
+        override fun areItemsTheSame(oldItem: TravelDetail, newItem: TravelDetail) =
+            oldItem.name == newItem.name
+
+        override fun areContentsTheSame(oldItem: TravelDetail, newItem: TravelDetail) =
+            oldItem == newItem
+    }
+
+}
--- app/src/main/java/domain/model/travel/TravelDetail.kt
+++ app/src/main/java/domain/model/travel/TravelDetail.kt
@@ -11,7 +11,8 @@ data class TravelDetail(
     val link: String,
     val cities: List<City>,
     val images: String,
-    val video: String
+    val video: String,
+    val viewType: Int = 0
 ) {
     companion object {
         val caribbean = "https://www.konnecthq.com/caribbean-sea/"
@@ -21,6 +22,14 @@ data class TravelDetail(
                         "there was shark there and that shark wanted to eat me but fortunately somehow I ran away", GOOGLE_URL, City.getFakes(), caribbean, " https ://persian5.cdn.asset.aparat.com/aparat-video/bdaea06a8c07f580a463d150bb9730af44790032-240p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImQxNWVmMmM1NDgxMGQzMDIzYTkzZDZlM2IwNzIyOGNlIiwiZXhwIjoxNjU4MjQ4NjEzLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.Y121L3MRjZjjxywwgAulf1gaeaClRRB0fs4LPoh1NA0"
             )
         }
+
+        const val VIEW_TYPE_COVER = 0
+        const val VIEW_TYPE_IMAGE = 1
+        const val VIEW_TYPE_DESCRIPTION = 2
+        const val VIEW_TYPE_LINK = 3
+        const val VIEW_TYPE_VIDEO = 4
+        const val VIEW_TYPE_BOOKMARK = 5
+
     }
 
 }
